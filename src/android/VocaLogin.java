3cd18521d91740c96ff337a377bc5113a69ff0ec
crossee <a01064297102@gmail.com>
2017-11-08 15:37:55 +0900

    Update VocaLogin.java

--- src/android/VocaLogin.java
+++ src/android/VocaLogin.java
@@ -14,27 +14,12 @@ import org.json.JSONObject;
  */
 
 public class VocaLogin extends CordovaPlugin {
-    private java.lang.String userId;
-    private java.lang.String userName;
-    private java.lang.String userClass;
-    private java.lang.String userCid;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("inputUserInfo")) {
-            this.userId = args.getString(0);
-            this.userName = args.getString(1);
-            this.userClass = args.getString(2);
-            this.userCid = args.getString(3);
-
-            String res = "{userid:" + this.userId
-                    + ",username:" + this.userName
-                    + ",userclass:" + this.userClass
-                    + ",usercid:" + this.userCid
-                    + "}";
-            JSONObject obj = new JSONObject(res);
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, obj));
+            
             return true;
         }
 
