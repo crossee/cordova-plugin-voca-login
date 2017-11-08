609d99d741761fcdc8e706c2dda7e02df08302a3
crossee <a01064297102@gmail.com>
2017-11-08 15:38:21 +0900

    Update CDVVocaLogin.m

--- src/ios/CDVVocaLogin.m
+++ src/ios/CDVVocaLogin.m
@@ -10,23 +10,7 @@
 
 - (void) inputUserInfo:(CDVInvokedUrlCommand *)command
 {
-    self.userId = [command.arguments objectAtIndex:0];
-    self.userName = [command.arguments objectAtIndex:1];
-    self.userClass = [command.arguments objectAtIndex:2];
-    self.userCid = [command.arguments objectAtIndex:3];
 
-    NSDictionary *jsonInfo = @{@"userid":self.userId,
-                               @"username":self.userName,
-                               @"userclass":self.userClass,
-                               @"usercid":self.userCid
-                               };
-
-    CDVPluginResult* pluginResult = [ CDVPluginResult
-                                     resultWithStatus    : CDVCommandStatus_OK
-                                     messageAsDictionary : jsonInfo
-                                     ];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
